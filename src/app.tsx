a4ea48ab7197f365f626b21a99931b01e0ed2bde
Cyriaque <cyriaquedelaunay@gmail.com>
2021-11-12 16:56:50 +0100

    chore: added base files

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import './app.scss';
+
+(window as any).tesla = {
+  App: {
+    DSServices: {
+      base_url: 'https://www.tesla.com',
+      configurator_url: 'https://www.tesla.com/model3/design',
+      cr_enabled: false,
+      cr_ip_check_enabled: false,
+      KeyManager: {},
+    },
+    debug: null,
+  },
+  base_url: 'https://www.tesla.com',
+  configurator_url: 'https://www.tesla.com/model3/design',
+  cr_enabled: false,
+  cr_ip_check_enabled: false,
+};
+function App() {
+  return (
+    <div className="app">
+      <h1>Tesla Website Dummy</h1>
+    </div>
+  );
+}
+
+export default App;
