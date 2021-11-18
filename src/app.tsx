d3fce01fcc3f5ecee325a6bb51a8f42adf860892
Cyriaque <cyriaquedelaunay@gmail.com>
2021-11-18 14:36:31 +0100

    chore: updated store object path

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,14 @@
 import './app.scss';
 
 (window as any).tesla = {
-  App: {
-    DSServices: {
-      base_url: 'https://www.tesla.com',
-      configurator_url: 'https://www.tesla.com/model3/design',
-      cr_enabled: false,
-      cr_ip_check_enabled: false,
-      KeyManager: {},
-    },
-    debug: null,
+  DSServices: {
+    base_url: 'https://www.tesla.com',
+    configurator_url: 'https://www.tesla.com/model3/design',
+    cr_enabled: false,
+    cr_ip_check_enabled: false,
+    KeyManager: {},
   },
+  debug: null,
   base_url: 'https://www.tesla.com',
   configurator_url: 'https://www.tesla.com/model3/design',
   cr_enabled: false,
